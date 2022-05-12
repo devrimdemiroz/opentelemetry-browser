812af05769117466cdce7bf0733dfc643a93278d
devrimdemiroz <devrim.demiroz@gmail.com>
2022-05-12 21:22:25 +0200

    otelcol visualizer - works for the given sample, need to feed more

--- docs/GeneratedDiagram.md
+++ docs/GeneratedDiagram.md
@@ -4,36 +4,64 @@ classDef tclass fill:orange,stroke:orange,stroke-width:2px,color:#fff
 classDef mclass fill:blue,stroke:blue,stroke-width:2px,color:#fff
 subgraph otelcol
 direction LR
-subgraph pipeline_traces
-subgraph receivers_traces
-M0((otlp)):::mclass
-end
-subgraph processors_traces
-M1((spanmetrics)):::mclass
-M2((batch)):::mclass
-end
-subgraph exporters_traces
-M3((otlp)):::mclass
-end
-end
-subgraph pipeline_metrics/spanmetrics
-subgraph receivers_metrics/spanmetrics
-M4((otlp/spanmetrics)):::mclass
-end
-subgraph exporters_metrics/spanmetrics
-M5((prometheus)):::mclass
-end
-end
-subgraph pipeline_metrics
-subgraph receivers_metrics
-M6((otlp)):::mclass
-M7((prometheus)):::mclass
-end
-subgraph exporters_metrics
-M8((prometheusremotewrite)):::mclass
-M9((prometheusremotewrite/1)):::mclass
-end
-end
-te(otlp)
+  subgraph pipeline_traces
+  direction LR
+      subgraph receivers_traces
+      direction LR
+          otlp1((T)):::tclass
+      end
+      otlp0((T)):::tclass-->|otlp|otlp1((T)):::tclass
+        linkStyle 0 stroke:orange,stroke-width:3px
+      subgraph processors_traces
+      direction LR
+          spanmetrics2[[spanmetrics]]
+          batch3[[batch]]
+      end
+      subgraph exporters_traces
+      direction LR
+          otlp5((T)):::tclass
+      end
+      otlp5((T)):::tclass-->|otlp|otlp4((T)):::tclass
+        linkStyle 1 stroke:orange,stroke-width:3px
+  receivers_traces----->processors_traces----->exporters_traces
+  end
+  subgraph pipeline_metrics/spanmetrics
+  direction LR
+      subgraph receivers_metrics/spanmetrics
+      direction LR
+          otlp/spanmetrics7((M)):::mclass
+      end
+      otlp/spanmetrics6((M)):::mclass-->|otlp/spanmetrics|otlp/spanmetrics7((M)):::mclass
+        linkStyle 4 stroke:blue,stroke-width:3px
+      subgraph exporters_metrics/spanmetrics
+      direction LR
+          prometheus9((M)):::mclass
+      end
+      prometheus9((M)):::mclass-->|prometheus|prometheus8((M)):::mclass
+        linkStyle 5 stroke:blue,stroke-width:3px
+  receivers_metrics/spanmetrics----->exporters_metrics/spanmetrics
+  end
+  subgraph pipeline_metrics
+  direction LR
+      subgraph receivers_metrics
+      direction LR
+          otlp11((M)):::mclass
+          prometheus13((M)):::mclass
+      end
+      otlp10((M)):::mclass-->|otlp|otlp11((M)):::mclass
+        linkStyle 7 stroke:blue,stroke-width:3px
+prometheus12((M)):::mclass-->|prometheus|prometheus13((M)):::mclass
+        linkStyle 8 stroke:blue,stroke-width:3px
+      subgraph exporters_metrics
+      direction LR
+          prometheusremotewrite15((M)):::mclass
+          prometheusremotewrite/117((M)):::mclass
+      end
+      prometheusremotewrite15((M)):::mclass-->|prometheusremotewrite|prometheusremotewrite14((M)):::mclass
+        linkStyle 9 stroke:blue,stroke-width:3px
+prometheusremotewrite/117((M)):::mclass-->|prometheusremotewrite/1|prometheusremotewrite/116((M)):::mclass
+        linkStyle 10 stroke:blue,stroke-width:3px
+  receivers_metrics----->exporters_metrics
+  end
 end
 ```
--- src/main/java/one/tractatus/otelcolvisualizer/OtelcolVisualizer.java
+++ src/main/java/one/tractatus/otelcolvisualizer/OtelcolVisualizer.java
@@ -11,6 +11,9 @@ public class OtelcolVisualizer {
 
     private static JsonNode pipelines;
     private static int seq;
+    private static int cseq;
+    private static BufferedWriter bw;
+    private static String pipeLineEnd="";
 
     public static void main(String[] args) throws IOException {
         readConfig();
@@ -22,7 +25,7 @@ public class OtelcolVisualizer {
 
         File file = new File("docs/GeneratedDiagram.md");
         FileWriter fw = new FileWriter(file);
-        BufferedWriter bw = new BufferedWriter(fw);
+        bw = new BufferedWriter(fw);
         bw.write("```mermaid"); bw.newLine();
         bw.write("flowchart TD");        bw.newLine();
         bw.write("classDef tclass fill:orange,stroke:orange,stroke-width:2px,color:#fff");        bw.newLine();
@@ -32,8 +35,8 @@ public class OtelcolVisualizer {
 
 
         addPipelines(bw);
-        bw.write("te(otlp)");bw.newLine();
-        bw.write("end");bw.newLine();
+        bw.write("end");
+        bw.newLine();
         bw.write("```");
         bw.close();
 
@@ -43,40 +46,107 @@ public class OtelcolVisualizer {
     private static void addPipelines(BufferedWriter bw) throws IOException {
         for (Iterator<String> it = pipelines.fieldNames(); it.hasNext(); ) {
             String pipeName = it.next();
-            System.out.println(pipeName);
-            bw.write("subgraph pipeline_"+pipeName);bw.newLine();
+            System.out.println("    pipeName="+pipeName);
+            bw.write("  subgraph pipeline_"+pipeName);bw.newLine();
+            bw.write("  direction LR\n");
             JsonNode pipeline = pipelines.get(pipeName);
             addPipeline(pipeName,pipeline, bw);
 
             // to end ("subgraph pipeline_"+pipeName)
-            bw.write("end");bw.newLine();
+            bw.write("  end\n");
         }
     }
 
     private static void addPipeline(String pipeName, JsonNode pipeline, BufferedWriter bw) throws IOException {
-        for (Iterator<String> it = pipeline.fieldNames(); it.hasNext(); ) {
-            String itemName = it.next();
-            System.out.println(itemName);
-            bw.write("subgraph "+itemName+"_"+pipeName);bw.newLine();
-            JsonNode worker = pipeline.get(itemName);
-            addWorkers(itemName,worker,bw);
-            // to end subgraph
-            bw.write("end");bw.newLine();
+        String receivers = addReceivers(pipeName, pipeline.get("receivers"));
+        String processors = addProcessors(pipeName, pipeline.get("processors"));
+        String exporters = addExporters(pipeName, pipeline.get("exporters"));
+        String connections="";
+        connections+=receivers+"----->";
+        cseq++;
+        if (processors!=null) {
+            connections+=processors+"----->";
+            cseq++;
         }
+        connections+=exporters;
+        bw.write("  "+connections+"\n");
     }
 
-    private static void addWorkers(String itemName, JsonNode worker, BufferedWriter bw) throws IOException {
-        for (JsonNode work : worker) {
 
-            System.out.println("---"+work.asText());
-            bw.write("M"+ seq++ +"(("+work.asText()+")):::mclass");bw.newLine();
+
+    private static String addProcessors(String pipeName, JsonNode processors) throws IOException {
+        if (processors==null) return null;
+
+        String name = "processors_" + pipeName;
+        bw.write("      subgraph " + name + "\n");
+        bw.write("      direction LR\n");
+        for (JsonNode processor : processors) {
+            bw.write("          "+processor.asText() + seq++ + "[["+processor.asText()+"]]\n");
+        }
+        bw.write("      end\n");
+        return name;
+    }
+
+    private static String addExporters(String pipeName, JsonNode exporters) throws IOException {
+        String name = "exporters_" + pipeName;
+        bw.write("      subgraph " + name + "\n");
+        bw.write("      direction LR\n");
+        String p0 = null;String p1 = null;String p2 = null;
+        String connections="";
+        for (JsonNode exporter : exporters) {
+            System.out.println("---" + exporter.asText());
+            p0 = exporter.asText();
+            p1 = getFormatted(pipeName, exporter);
+            p2 = getFormatted(pipeName, exporter);
+            bw.write("          "+p2+"\n");
+            connections += p2 + "-->|" + p0 + "|" + p1+"\n";
+            connections += "        linkStyle "+ cseq++ + getFormatted(pipeName);
+        }
+        bw.write("      end\n");
+        bw.write("      "+connections);
+        return name;
+    }
+    private static String addReceivers(String pipeName, JsonNode receivers) throws IOException {
+        String name = "receivers_" + pipeName;
+        bw.write("      subgraph " + name+"\n");
+        bw.write("      direction LR\n");
+        String p0 = null;String p1 = null;String p2 = null;
+        String connections="";
+        for (JsonNode receiver : receivers) {
+
+            System.out.println("---" + receiver.asText());
+            p0 = receiver.asText();
+            p1 = getFormatted(pipeName, receiver);
+            p2 = getFormatted(pipeName, receiver);
+
+            bw.write("          "+p2+"\n");
+
+            //otlp0((T)):::tclass -->|oltp| otlp1 would be written outside end
+            connections += p1 + "-->|" + p0 + "|" + p2+"\n";
+            connections += "        linkStyle "+ cseq++ + getFormatted(pipeName);
 
         }
+        // to end subgraph
+        bw.write("      end\n");
+        bw.write("      "+connections);
 
-            // to end subgraph
-            //bw.write("end");bw.newLine();
-
+        return name;
     }
+
+    private static String getFormatted(String pipeName) {
+        if (pipeName.matches("traces")){
+            return " stroke:orange,stroke-width:3px\n";
+        }
+        return " stroke:blue,stroke-width:3px\n";
+    }
+
+    private static String getFormatted(String pipeName, JsonNode jsonNode) {
+        if (pipeName.matches("traces")){
+            return jsonNode.asText() + seq++ + "((T)):::tclass";
+        }
+        return jsonNode.asText() + seq++ + "((M)):::mclass";
+    }
+
 
     private static void readConfig() throws IOException {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
